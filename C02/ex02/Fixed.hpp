43f4b4a5894afbea14af54577d9dcc0faca2feae
Сергей Сысоев <bleaf@MacBook-Pro-Sergej.local>
2022-10-27 14:24:44 +0300

    done

--- C02/ex02/Fixed.hpp
+++ C02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: bleaf <bleaf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:11:23 by bleaf             #+#    #+#             */
-/*   Updated: 2022/10/27 14:24:15 by bleaf            ###   ########.fr       */
+/*   Updated: 2022/10/27 14:14:54 by bleaf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,37 @@ public:
 
 	Fixed &operator=(Fixed const &other);
 
+	bool operator>(Fixed const &other) const;
+	bool operator<(Fixed const &other) const;
+	bool operator>=(Fixed const &other) const;
+	bool operator<=(Fixed const &other) const;
+	bool operator==(Fixed const &other) const;
+	bool operator!=(Fixed const &other) const;
+
+	Fixed operator+(Fixed const &other);
+	Fixed operator-(Fixed const &other);
+	Fixed operator*(Fixed const &other);
+	Fixed operator/(Fixed const &other);
+
+	Fixed operator++(int);
+	Fixed operator++();
+	Fixed operator--(int);
+	Fixed operator--();
+
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 	int toInt(void) const;
 	float toFloat(void) const;
-
+	static Fixed const &min(Fixed const &a, Fixed const &b);
+	static Fixed const &max(Fixed const &a, Fixed const &b);
+	static Fixed &min(Fixed &a, Fixed &b);
+	static Fixed &max(Fixed &a, Fixed &b);
 };
 
 std::ostream &operator<<(std::ostream &out, Fixed const &value);
+Fixed const &min(Fixed const &a, Fixed const &b);
+Fixed const &max(Fixed const &a, Fixed const &b);
+Fixed &min(Fixed &a, Fixed &b);
+Fixed &max(Fixed &a, Fixed &b);
 
 #endif
--- C02/ex02/main.cpp
+++ C02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bleaf <bleaf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:53:38 by bleaf             #+#    #+#             */
-/*   Updated: 2022/10/27 14:24:17 by bleaf            ###   ########.fr       */
+/*   Updated: 2022/10/27 14:19:57 by bleaf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 #include <iostream>
 int main( void ) {
 	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-return 0;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << max( a, b ) << std::endl;
+	return 0;
 }
