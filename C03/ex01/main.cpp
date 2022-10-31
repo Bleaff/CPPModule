c9d881e06c7c9f3702c21b4c16d799f007bdd5f6
Сергей Сысоев <bleaf@MacBook-Pro-Sergej.local>
2022-10-31 23:51:23 +0300

    story

--- C03/ex01/main.cpp
+++ C03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bleaf <bleaf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 20:49:56 by bleaf             #+#    #+#             */
-/*   Updated: 2022/10/31 19:18:52 by bleaf            ###   ########.fr       */
+/*   Updated: 2022/10/31 23:49:16 by bleaf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@
 
 int main(void)
 {
+	std::cout << "Now you will witness an incident in a bar between Django and the target he has been hunting for 3 months." << std::endl;
+	ClapTrap *greengo = new ScavTrap("Django");
+	greengo->attack("$5,000 goal");
+	ScavTrap goal("$5,000 goal");
+	goal.takeDamage(greengo->getAD());
+	goal.attack("Django");
+	greengo->takeDamage(goal.getAD());
+	goal.takeDamage(greengo->getAD() * 5);
+	goal.beRepaired(30);
 	ScavTrap s("S");
 	s.attack("ad");
 	s.guardGate();
 	s.takeDamage(500);
 	s.beRepaired(10);
 	s.attack("Sany");
-
+	delete greengo;
 	return (0);
 }
